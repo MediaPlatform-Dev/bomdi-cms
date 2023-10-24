7b1c59eb7d0a850c2b17be03690c181ae177444e
junhyuksung <mzseongjh@lgupluspartners.co.kr>
2023-10-24 11:22:33 +0900

    CMD-48 - 미사용 레포지토리 메서드 제거

--- src/main/java/com/megazone/act/cms/domain/repository/CorporationRepository.java
+++ src/main/java/com/megazone/act/cms/domain/repository/CorporationRepository.java
@@ -3,9 +3,5 @@ package com.megazone.act.cms.domain.repository;
 import com.megazone.act.cms.domain.entity.Corporation;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface CorporationRepository extends JpaRepository<Corporation, Long> {
-
-    Optional<Corporation> findByName(String name);
 }
