067fa6cbf2ff56259d63f54639343123db50949e
junhyuksung <mzseongjh@lgupluspartners.co.kr>
2023-10-24 14:37:34 +0900

    CMD-48 SQL 스크립트에 entity 변경 반영

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -23,12 +23,12 @@ values ('이유종'),
 insert into tb_cntrct_m(cntrct_nm, rmrk,
                         strt_ymd, end_ymd,
                         cntrct_gb1_cd, cntrct_clas_cd, src_system_cd,
-                        currency_unit_type, amt, tot_amt)
+                        currency_unit_type, amt, tot_amt, has_vat)
 values ('매출 테스트 계약', '매출 테스트 설명',
         '2023-01-01', '2023-12-01',
         'SALES', 'CONTRACT', 'SALESFORCE',
-        'KRW', 3000, 3000),
-       ('매입 테스트 계약 2', '매입 테스트 설명 2',
+        'KRW', 3000, 3000, true),
+       ('매출 테스트 계약 2', '매출 테스트 설명 2',
         '2023-01-01', '2023-12-01',
-        'PURCHASE', 'CONTRACT', 'SALESFORCE',
-        'KRW', 1000, 1000);
+        'SALES', 'CONTRACT', 'SALESFORCE',
+        'KRW', 1000, 1000, true);
